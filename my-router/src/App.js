6fd658bcab2b9da9f6d997173da6fa916b1a7a04
James McNeil <mcneil_james@hotmail.com>
2022-01-03 12:13:04 +0000

    Added NavLink

--- my-router/src/App.js
+++ my-router/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 import './App.css'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -10,6 +10,9 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>My Articles</h1>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
         </nav>
         <Switch>
           <Route exact path='/'>
