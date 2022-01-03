4303ea93ece738d51a32e6182cec472c37ff2aa6
James McNeil <mcneil_james@hotmail.com>
2022-01-03 18:36:26 +0000

    npm i react-router-dom@5.2.0

--- my-router/src/pages/Article.js
+++ my-router/src/pages/Article.js
@@ -1,7 +1,10 @@
+import { useParams } from 'react-router-dom'
 const Article = () => {
+  const { id } = useParams()
+
   return (
     <div>
-      <h1>Article</h1>
+      <h1>Article - {id} </h1>
     </div>
   )
 }
