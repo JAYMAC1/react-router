73b9427895875398a48330e797598080948b707c
James McNeil <mcneil_james@hotmail.com>
2022-01-03 14:21:31 +0000

    used custom hook to fetch data and output to screen with link to article route

--- my-router/src/pages/Home.js
+++ my-router/src/pages/Home.js
@@ -1,11 +1,25 @@
+import { Link } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
+
+//  Styles
+import './Home.css'
+
 const Home = () => {
+  const url = 'http://localhost:3000/articles'
+  const { data: articles, isPending, error } = useFetch(url)
   return (
-    <div>
-      <h2>Home Page</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet aliquid
-        veritatis fuga tempora inventore iste deleniti, et ex aperiam ab.
-      </p>
+    <div className='home'>
+      <h2>Articles</h2>
+      {isPending && <div>Loading....</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className='card'>
+            <h2>{article.title}</h2>
+            <p>{article.author}</p>
+            <Link to={`/articles/${article.id}`}>Read more...</Link>
+          </div>
+        ))}
     </div>
   )
 }
