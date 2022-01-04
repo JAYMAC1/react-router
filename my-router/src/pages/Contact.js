06b5dab9a7a0cb15225c3455a9c763341195461e
James McNeil <mcneil_james@hotmail.com>
2022-01-04 08:53:50 +0000

    used useLocation hook with URLSearchParams to extract query string data

--- my-router/src/pages/Contact.js
+++ my-router/src/pages/Contact.js
@@ -1,7 +1,14 @@
+import { useLocation } from 'react-router-dom'
+
 const Contact = () => {
+  const queryString = useLocation().search
+
+  // javascript not react
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get('name')
   return (
     <div>
-      <h2>Contact Page</h2>
+      <h2>Contact - Hello {name}</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet aliquid
         veritatis fuga tempora inventore iste deleniti, et ex aperiam ab.
